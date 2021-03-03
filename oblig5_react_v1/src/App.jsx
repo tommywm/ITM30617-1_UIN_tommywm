492aff29d8ae0cddac54acb0c4c0c7c3163e0ad1
Tommy Waglen Major <43408994+tommywm@users.noreply.github.com>
2021-03-03 12:22:40 +0100

    🔥  DONE! (converted to webpack)

---
+++ oblig5_react_v1/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import Button from './components/Button';
+import Food from './components/Food';
+import MyComponent from './components/MyComponent';
+import Wrapper from './components/Wrapper';
+
+import { food } from './assets/js/food';
+
+// import './assets/scss/styles.scss';
+
+const App = () => {
+  const [alertMessage, setAlertMessage] = useState('');
+  const handleAlertMessage = (message) => {
+    setAlertMessage(message.messagevalue);
+  };
+
+  return (
+    <>
+      <Wrapper displayType="flex">
+        <MyComponent title="🔥 It works! 🔥" />
+        <Food food={food} title="The food" />
+        <Alert
+          handleAlertMessage={handleAlertMessage}
+          alertMessage={alertMessage}
+        />
+        <Button alertMessage={alertMessage} />
+      </Wrapper>
+    </>
+  );
+};
+
+export default App;
---
+++ oblig5_react_v1/src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
+
+const MyComponent = ({ title }) => (
+  <>
+    <Title title={title} />
+  </>
+);
+
+MyComponent.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default MyComponent;
